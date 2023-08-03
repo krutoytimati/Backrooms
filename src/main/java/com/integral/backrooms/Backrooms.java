6a388579d3692d1418be476acc5b3dd0b6d685dd
Elizabeth Avergini <106208768+ElizabethAvergini@users.noreply.github.com>
2023-08-03 20:00:55 +0300

    Update Backrooms.java

--- src/main/java/com/integral/backrooms/Backrooms.java
+++ src/main/java/com/integral/backrooms/Backrooms.java
@@ -25,13 +25,13 @@ import com.integral.backrooms.proxy.CommonProxy;
 
 @Mod(modid = Backrooms.MODID, version = Backrooms.VERSION, name = Backrooms.NAME)
 public class Backrooms {
-    public static final String MODID = "assets/backrooms";
+    public static final String MODID = "backrooms";
     public static final String NAME = "Backrooms Mod";
     public static final String VERSION = "1.0.0";
 
     public static SimpleNetworkWrapper packetHandler;
 
-    @SidedProxy(clientSide = "com.integral.backrooms.proxy.ClientProxy", serverSide = "com.integral.backrooms.proxy.CommonProxy")
+    @SidedProxy(clientSide = "com.forohon.backrooms.proxy.ClientProxy", serverSide = "com.forohon.backrooms.proxy.CommonProxy")
     public static CommonProxy proxy;
 
     public static final Logger logger = LogManager.getLogger("Backrooms");
